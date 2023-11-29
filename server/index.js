cff352117d9cb85c2f4e0be61f34251e6a41f080
maletha <91272338+AnuragMaletha@users.noreply.github.com>
2023-11-30 00:05:34 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const app=express();
+const mongoose =require("mongoose");
+const dotenv =require("dotenv");
+const Route=require("./routes")
+const cors = require("cors")
+
+app.use(cors({
+  origin:"http://localhost:5173"
+}))//to allow all requests from this address
+
+
+dotenv.config();
+const PORT= process.env.PORT || 8800
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(()=>console.log("DB connected")).catch((err)=>console.log(err)); 
+
+  app.use(express.json());
+  
+app.use("/api/tasks",Route);
+
+app.listen(PORT,()=>{
+    console.log(`app running on ${PORT}`)
+})
+
